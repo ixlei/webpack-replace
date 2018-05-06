a0dd0f2677487dbd69d5ffb91047d94147108861
ixlei <1099461262@qq.com>
2018-05-06 23:15:06 +0800

    init

---
+++ index.js
@@ -0,0 +1,84 @@
+const ConcatSource = require('webpack-core/lib/ConcatSource');
+
+class ReplaceWebapck {
+    constructor(options) {
+        this.options = {
+            inject: options.inject
+        }
+    }
+
+    getAllChunks(compilation) {
+        const chunkOnlyFilterConfig = {
+            assets: false,
+            cached: false,
+            children: false,
+            chunks: true,
+            chunkModules: false,
+            chunkOrigins: false,
+            errorDetails: false,
+            hash: false,
+            modules: false,
+            reasons: false,
+            source: false,
+            timings: false,
+            version: false
+        };
+        const allChunks = compilation.getStats().toJson(chunkOnlyFilterConfig).chunks;
+    }
+
+    apply(compiler) {
+        let optimizeChunkAssetsHook = (chunks) => {
+            let allChunks = this.getAllChunks(this.compilation);
+            allChunks.forEach((item, key) => {
+                let js = [],
+                    css = [];
+                item.files.forEach(function(file) {
+                    if (/\.js$/.test(file)) {
+                        js.push(file)
+                    }
+                    if (/.css$/.test(file)) {
+                        css.push(file)
+                    }
+                });
+                let jsSource = [],
+                    cssSource = [];
+                js.forEach(function(item) {
+                    jsSource.push(compilation.assets[item].source());
+                });
+                css.forEach(function(item) {
+                    cssSource.push(compilation.assets[item].source())
+                });
+
+                this.options.inject && this.options.inject(item.names, jsSource, cssSource);
+                if (this.options.inject) {
+                    jsSource.forEach(function(source, index) {
+                        compilation.assets[js[index]] = new ConcatSource(source)
+                    })
+
+                    cssSource.forEach(function(source, index) {
+                        compilation.assets[css[index]] = new ConcatSource(source)
+                    })
+                }
+            })
+        }
+
+        let compilationHook = (compilation, params) => {
+            this.compilation = compilation;
+            compilation.hooks.optimizeChunkAssets.tap('webpack_replace', optimizeChunkAssetsHook);
+        }
+
+        if (compiler.hooks) {
+            compiler.hooks.thisCompilation.tap('webpack_replace', compilationHook);
+        } else {
+            compiler.plugin('compilation', (compilation) => {
+                compilation.plugin('optimize-chunk-assets', (chunks, done) => {
+                    this.optimizeChunkAssetsHook(chunks);
+                    done();
+                })
+            })
+        }
+
+    }
+}
+
+module.exports = ReplaceWebapck;
